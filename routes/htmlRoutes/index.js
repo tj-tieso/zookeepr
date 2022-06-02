eb7b65da257889c1eb86af4137f5e25c2b630e4f
tj-tieso <tj.tieso@gmail.com>
2022-06-02 11:10:23 -0500

    add zookeeper

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,20 +2,20 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
 router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
-// wildcard route
+// wild card route
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
